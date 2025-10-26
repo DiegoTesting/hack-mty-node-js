2c82c562afc7b04ff951622f4ffc73e563f739c2
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-10-26 06:37:06 -0600

    as

--- routes/openrouter.routes.js
+++ routes/openrouter.routes.js
@@ -20,35 +20,6 @@ const ClienteService = require('../services/cliente.service');
 const cliente_service = new ClienteService();
 
 
-// endpoint para IA de OpenRouter
-router.get("/", async (req, res) => {
-  try {
-    const response = await axios.post(
-      "https://openrouter.ai/api/v1/chat/completions",
-      {
-        model: "openai/gpt-4o",
-        messages: [
-          { role: "user", content: "qb chotaa sos" }
-        ],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    // Extraemos la respuesta del asistente
-    const botMessage = response.data.choices?.[0]?.message?.content || "No hay respuesta";
-
-    res.json({ botMessage, raw: response.data });
-  } catch (err) {
-    console.error("Error probando OpenRouter:", err.response?.data || err.message);
-    res.status(500).json({ error: "Error en OpenRouter", details: err.response?.data || err.message });
-  }
-});
-
 
 router.post("/",validacionJWT, async (req, res) => {
   const { message } = req.body;
